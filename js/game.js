a512aa827fd0031ad47f3c54348cbec527bf40b5
Moonbench <noreply@moonbench.xyz>
2017-10-10 20:00:14 -0700

    phases are scens

--- js/game.js
+++ js/game.js
@@ -14,13 +14,13 @@ const Game = (function(){
     game.engine.set_world(world);
   }
 
-  function set_phase(game, phase){
-    set_world(game, phase.world);
-    game.load_assets(phase.asset_loading_function);
-    game.load_entities(phase.entity_loading_function);
-    game.engine.handle_key = phase.handle_key;
-    game.engine.handle_mouse_move = phase.handle_mouse_move;
-    game.engine.handle_mouse_button = phase.handle_mouse_button;
+  function set_scene(game, scene){
+    set_world(game, scene.world);
+    game.load_assets(scene.asset_loading_function);
+    game.load_entities(scene.entity_loading_function);
+    game.engine.handle_key = scene.handle_key;
+    game.engine.handle_mouse_move = scene.handle_mouse_move;
+    game.engine.handle_mouse_button = scene.handle_mouse_button;
   }
 
   return {
@@ -31,7 +31,7 @@ const Game = (function(){
 
       game.load_assets = function(load_function){ load_assets(game.engine, load_function) };
       game.load_entities = function(load_function){ load_entities(game.world, game.engine.assets, load_function) };
-      game.set_phase = function(phase){ set_phase(game, phase) };
+      game.set_scene = function(scene){ set_scene(game, scene) };
 
       game.run = function(){ game.engine.run() };
       return game;
