046cbbfb1201c773aa568450b770dd1177fcc53a
Acacia <acacia@debian>
2017-10-15 20:34:07 -0700

    Add the ability to tile images on a sprite

--- js/entities/sprite.js
+++ js/entities/sprite.js
@@ -4,15 +4,21 @@ const Sprite = (function(){
 
   function render(entity, ctx, dt){
     if(entity.asset)
-      ctx.drawImage(entity.asset, -entity.height/2, -entity.width/2, entity.height, entity.width);
+      if(entity.repeat){
+        ctx.fillStyle = ctx.createPattern(entity.asset, entity.repeat);
+        ctx.fillRect(-entity.height/2, -entity.width/2, entity.height, entity.width);
+      } else {
+        ctx.drawImage(entity.asset, -entity.height/2, -entity.width/2, entity.height, entity.width);
+      }
     else {
       ctx.fillStyle = "#222222";
       ctx.fillRect(-entity.height/2, -entity.width/2, entity.height, entity.width);
     }
   }
 
-  function extend(entity, asset){
+  function extend(entity, asset, repeat){
     entity.asset = asset;
+    entity.repeat = repeat;
 
     const parent_render = entity.render;
     entity.render = function(ctx, dt){ render(entity, ctx, dt); parent_render(ctx, dt) };
@@ -21,8 +27,8 @@ const Sprite = (function(){
   }
 
   return {
-    create: function(x, y, width, height, angle, asset){
-      return extend(Entity.create(x, y, width, height, angle), asset);
+    create: function(x, y, width, height, angle, asset, repeat){
+      return extend(Entity.create(x, y, width, height, angle), asset, repeat);
     },
     extend,
   }
