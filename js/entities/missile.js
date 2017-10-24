7cf64ee19b9618080898f5717fffcbdf28961bca
Acacia <acacia@debian>
2017-10-24 14:58:12 +1100

    Add basic gamepad support
    This replaces the boolean state values for direction with normalized
    0-1 values.
    
    It then adds support to query the gamepad during each update loop
    (if the gamepad has been connected) and update the states from that.

--- js/entities/missile.js
+++ js/entities/missile.js
@@ -4,7 +4,7 @@ const MissileEntity = (function(){
   function update(entity, dt){
     entity.age += dt;
     if(entity.age > entity.max_age){
-      entity.state.forward = false;
+      entity.state.forward = 0;
       entity.dead = true;
     }
   }
@@ -34,12 +34,15 @@ const MissileEntity = (function(){
 
   function extend(entity, velocity){
     entity.is_projectile = true;
-    entity.state.forward = true;
+    entity.state.forward = 1;
     entity.age = 0;
     entity.max_age = 2.5;
     entity.parent = null;
     entity.vector.magnitude = 750;
     entity.vector.angle = entity.angle;
+
+    const parent_update = entity.update;
+    entity.update = function(dt){ update(entity, dt); parent_update(dt) };
 
     const parent_check_collision_against = entity.check_collision_against;
     entity.check_collision_against = function(other_entity, no_checkback){
