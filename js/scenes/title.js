d3eef4f0935460cb18fe4681b9bc0729ebc46c09
Acacia <acacia@debian>
2017-11-11 01:27:57 +1100

    Add easing transitions to moveable entities
    This gives moveable entities the ability to be set to transition
    to a specific point within a specific time range.
    
    A demo is added to the title

--- js/scenes/title.js
+++ js/scenes/title.js
@@ -1,10 +1,25 @@
 const title_scene = (function(){
+  const world = World.create(game.engine.viewport.width, game.engine.viewport.height);
+  let title;
+
+  const states = {
+    opening: 0,
+    loading_menu: 1,
+    showing_menu: 2
+  };
+  let state = null;
+
   function start_game(){
     game.set_scene(level_scene);
   }
 
+  function transition_to_menu(){
+    state = states.loading_menu;
+    title.transition_to(0, -100, 1, Ease.inOutQuad);
+  }
+
   return {
-    world: World.create(game.engine.viewport.width, game.engine.viewport.height),
+    world,
 
     load_assets: function(engine){
       engine.assets.add_image("title_bg", "assets/titlebg.png");
@@ -13,9 +28,7 @@ const title_scene = (function(){
     },
 
     load_entities: function(world, assets){
-      const bg = MoveableEntity.extend(
-        Sprite.create(0, 0, world.height*2, world.width*2, 0, 1, assets.images["title_bg"])
-      );
+      const bg = MoveableEntity.extend(Sprite.create(0, 0, world.height*2, world.width*2, 0, 1, assets.images["title_bg"]));
       bg.friction = 0;
       bg.state.rotate = 0.01;
       world.add_entity(bg, 10);
@@ -27,19 +40,27 @@ const title_scene = (function(){
       planet.state.rotate = 0.01;
       world.add_entity(planet, 75);
 
-      const title = Sprite.create(0, 0, world.height/1.5, world.width/1.5, 0, 1, assets.images["title"]);
+      title = MoveableEntity.extend(Sprite.create(0, -world.height/1.3, world.height/1.5, world.width/1.5, 0, 1, assets.images["title"]));
+      title.transition_to(0, 0, 2, Ease.outCubic);
       world.add_entity(title, 100);
     },
 
-    update: function(){},
+    update: function(engine, dt){
+      if(state == states.loading_menu && !title.transitioning)
+        state = states.showing_menu;
+
+      if(state == states.showing_menu){
+        start_game();
+      }
+    },
 
     handle_mouse_button: function(event, pressed){
-      if(pressed) start_game();
+      if(pressed) transition_to_menu();
     },
     handle_mouse_move: function(){},
 
     handle_gamepad: function(gamepad){
-      if(gamepad.buttons[0].pressed) start_game();
+      if(gamepad.buttons[0].pressed) transition_to_menu();
     },
   };
 })();
